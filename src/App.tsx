252f7fcff8b57040a65c203d6b23f826886e95ca
Benga <benga.olasebikan@gmail.com>
2021-03-20 21:16:38 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type FormElem = React.FormEvent<HTMLFormElement>
+
+// ! Create a representation of the todos
+interface ITodo {
+  id: string,
+  todo: string,
+  isComplete: boolean,
 }
 
-export default App;
+const App = () => {
+  const [text, setText] = useState<string>('')
+  const [list, setList] = useState<ITodo[]>([])
+  const [error, SetError] = useState<string>('')
+
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault()
+    const newTodo: ITodo[] = [...list, {id: Math.random().toString(), todo: text, isComplete: false}]
+    if (!list.find(item => item.todo === text)) {
+      setList(newTodo)
+      SetError('')
+    } else {
+      SetError('Item already exists')
+    }
+    setText('')
+  }
+
+  const completeTask = (id: string): void => {
+    setList(list.map(todo => todo.id === id ? {...todo, isComplete: !todo.isComplete} : todo))
+  }
+
+  const deleteTask = (id: string): void => {
+    setList(list.filter(item => item.id !== id))
+  }
+
+
+  console.log(list)
+
+
+  return (
+    <Fragment>
+      <section id="form">
+        <h1>TO-DO LISTS</h1>
+        <div className="form">
+          {error && <p className="form__err">{error}</p>}
+        <form onSubmit={handleSubmit} >
+          <input onChange={e => setText(e.target.value)} value={text} type="text" name="todo" placeholder="Enter todo" className="form__input" required/>
+          <button className="form__btn">Add Todo</button>
+        </form>
+        </div>
+      </section>
+      <section id="todos">
+        <div className="todos">
+          {
+            list.length > 0 && list.map(item => <ul className="todos__list" key={item.id}>
+              <li className="todos__item">{item.todo}
+              {!item.isComplete ? <button onClick={() => completeTask(item.id)} className={`todos__btn ${item.isComplete ? 'complete': ''}`}>Complete</button> : <button onClick={() => completeTask(item.id)} className={`todos__btn ${item.isComplete ? 'complete': ''}`}>Undo Complete</button>}
+              <button onClick={() => deleteTask(item.id)} className="todos__dlt">Delete</button>
+              </li>
+            </ul>)
+          }
+        </div>
+      </section>
+    </Fragment>
+  )
+}
+
+export default App
+
